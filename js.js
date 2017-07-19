0a74bcb9a65d54ece19ffc30ff72cd3a04bea18f
Witherspoonmyles <Witherspoonmyles@gmail.com>
2017-07-19 17:20:22 -0500

    Stu

--- js.js
+++ js.js
@@ -1,3 +1,36 @@
-function StuPot() {
-    // run this
+var body = document.querySelector("body");
+
+var textbox = document.querySelector("div#textbox")
+
+var button = document.querySelector("button"); 
+
+var input = document.querySelector("input");
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function addMessage(message, className) {
+   textbox.insertAdjacentHTML("beforeend", `<p class="${className}"> ${message} </p>`);
+}
+
+function mascotReply() {
+    addMessage(`Ello, i'm ${mascot.name}.`, "mascotMessage");
+}
+
+// Greeting
+addMessage("Umm...Who are you?" , "welcomeMessage");
+
+
+button.addEventListener('click' , function() {
+    addMessage(input.value, "userMessage");
+    
+    setTimeout(mascotReply, 1500);
+});
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+var mascot = {
+    name: "2D",
+    age: 39
 }
